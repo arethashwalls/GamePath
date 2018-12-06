9ab321869f46357ca9c380b5b7b87e1e2f957450
ASH Walls <ariswalls@gmail.com>
2018-12-06 07:36:43 -0700

    More api work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -76,9 +76,9 @@ $(document).ready(function () {
     }
 
     const gameKey = 'f28275b8fb7b306cbb42f124b2e94066';
-    // const musicKey = 'a3e040df3cd2213704ea57b5d25c8714';
+    const musicKey = 'a3e040df3cd2213704ea57b5d25c8714';
 
-    function getGameIDs(mood, property, cb) {
+    function getGameIds(mood, property) {
         const moodTerms = searchTerms(mood);
         let gameQueryUrl = `https://api-2445582011268.apicast.io/${property}s/`;
         const ids = property === 'genre' ? moodTerms.genreIds : moodTerms.themeIds;
@@ -87,59 +87,67 @@ $(document).ready(function () {
         }
         gameQueryUrl = gameQueryUrl.substring(0, gameQueryUrl.length - 1);
         gameQueryUrl += '?fields=*&filter[rating][gt]=90&order=popularity:desc';
-        const gameEncodedUrl = encodeURIComponent(gameQueryUrl);
+        const gameSafeUrl = 'https://corsbridge.herokuapp.com/' + encodeURIComponent(gameQueryUrl);
         let gameIds = [];
-        $.ajax({
-            type: 'GET',
-            contentType: 'application/json',
-            headers: { 'user-key': gameKey, "Accept": "application/json" },
-            url: 'https://corsbridge.herokuapp.com/' + gameEncodedUrl,
-            success: async function (data) {
-                for(datum of data) {
-                    for(let i = 0; i < 10; i++) {
-                        gameIds.push(datum.games[i]);
-                    }
+        axios.get(gameSafeUrl, {
+            headers: {
+                'user-key': gameKey, 
+                "Accept": "application/json"
+            }
+        }).then(response => {
+            for (let datum of response.data) {
+                for (let i = 0; i < 10; i++) {
+                    gameIds.push(datum.games[i]);
                 }
-                let game = await cb(getRandom(gameIds))
-                console.log( game );
             }
         });
         return gameIds;
     }
 
-    function getGame(id){
-        let gameQueryUrl = `https://api-2445582011268.apicast.io/games/${id}?fields=*`;
-        const gameEncodedUrl = encodeURIComponent(gameQueryUrl);
-        $.ajax({
-            type: 'GET',
-            contentType: 'application/json',
-            headers: { 'user-key': gameKey, "Accept": "application/json" },
-            url: 'https://corsbridge.herokuapp.com/' + gameEncodedUrl,
-            success: function (data) {
-                let game = new Game(data[0].name, data[0].summary, data[0].platforms, data[0].cover, data[0].videos);
-                console.log(game);
+    //console.log( getGameIds('angry', 'theme') );
 
-                return game;
-                
-            }
-        });
+    async function getGames() {
+        //let games = [];
+        const gameIds = await getGameIds('angry', 'theme');
+        console.log(gameIds);
+
     }
 
-    console.log(getGameIDs('angry', 'genre', getGame));
+    getGames();
+
+    // function getGame(id) {
+    //     let gameQueryUrl = `https://api-2445582011268.apicast.io/games/${id}?fields=*`;
+    //     const gameEncodedUrl = encodeURIComponent(gameQueryUrl);
+    //     $.ajax({
+    //         type: 'GET',
+    //         contentType: 'application/json',
+    //         headers: { 'user-key': gameKey, "Accept": "application/json" },
+    //         url: 'https://corsbridge.herokuapp.com/' + gameEncodedUrl,
+    //         success: function (data) {
+    //             let game = new Game(data[0].name, data[0].summary, data[0].platforms, data[0].cover, data[0].videos);
+    //             console.log(game);
+
+    //             return game;
+
+    //         }
+    //     });
+    // }
+
+   // console.log(getGameIDs('angry', 'genre', getGame));
 
 
     // const gameQueryUrl = 'https://api-2445582011268.apicast.io/themes/?search=sandbox&fields=name';
     // const gameEncodedUrl = encodeURIComponent(gameQueryUrl);
 
-    // const musicQueryUrl = `http://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&artist=cher&api_key=${musicKey}&format=json`;
-    // const musicEncodedUrl = encodeURIComponent(musicQueryUrl);
+    const musicQueryUrl = `http://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&artist=cher&api_key=${musicKey}&format=json`;
+    const musicSafeUrl = 'https://corsbridge.herokuapp.com/' + encodeURIComponent(musicQueryUrl);
 
     $('.mood-buttons').on('click', '.mood-button', () => {
         const mood = $(this).attr('data-mood');
 
     });
 
-
+    // axios.get(musicSafeUrl).then(response => console.log(response));
 
     // $.ajax({
     //     type: 'GET',
